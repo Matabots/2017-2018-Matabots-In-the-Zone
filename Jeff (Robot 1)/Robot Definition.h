1d01e552880d19b0a78c0c07c7580b938949d286
Nathan Boyd <nboyd2226@gmail.com>
2017-10-20 23:28:15 -0700

    Added more robot initialization code
    Added code for dimensioning the robot

--- Jeff (Robot 1)/Robot Definition.h
+++ Jeff (Robot 1)/Robot Definition.h
@@ -7,14 +7,47 @@ struct Vector
 
 struct Chassis
 {
-	float wheelDiameter;	//center of rotation
-	Vector COR;	//center of rotation
-
+	float wheelDiameter;	//inches
+	Vector COR;	//center of rotation. x = 0, y = 0 at middle. + faces forward and right
 };
 
 struct Lift
 {
 	//define combination of bars
-	float armLength;
+	float armBotLength;
+	float armTopLength;
+	float theta;
+	float alpha;
+	float clawLength;
 	float height;
 };
+
+struct Robot
+{
+	Chassis chassis;
+	Lift lift;
+};
+
+Robot robot;
+
+void initializeRobot()
+{
+	robot.chassis.wheelDiameter = 4;
+	robot.chassis.COR.x = 0;
+	robot.chassis.COR.y = 0;
+	robot.lift.armBotLength = 0;
+	robot.lift.armTopLength = 0;
+	//robot.lift.theta = Avg(SensorValue[],SenorValue[]);
+	robot.lift.alpha = SensorValue[ClawLiftAngle];
+}
+
+float ClawEndpoint()
+{
+	//clawEndpoint = c1+c2+c3+(sin(theta)*(x1+x2))+(c3*sing(alpha))
+
+}
+
+int stackHeight()
+{
+
+}
